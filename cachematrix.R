bcfa4f4ba26a935ae85e36aa0af762a177a041d6
adaviscore <adavis@coreinformatics.com>
2014-12-17 20:36:17 -0500

    Assignment
    Created the methods that will cache and solve the inverse for a Matrix.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix will create a new matrix and set the getter and setter functions
+## cacheSolve will be given a List and then solve for the inverse of the Matrix. 
+## It will first check to see if the data has been cached; if so, then go and 
+## grab the data.
 
-## Write a short comment describing this function
+## This function takes a Matrix and sets it.
+## When you call this function all the functions get called
+## which then resets i and x appropriately.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() {x}
+  setInverse <- function(inverse) {i <<- inverse}
+  getInverse <- function() {i}
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+  
 }
 
-
-## Write a short comment describing this function
+## cacheSolve will be given a List and then solve for the inverse of the Matrix. 
+## It will first check to see if the data has been cached; if so, then go and 
+## grab the data. Once solved it will cache it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  i <- x$getInverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  m <- x$get()
+  i <- solve(m, ...)
+  x$setInverse(i)
+  i
 }
+
+bigMatrix <- makeCacheMatrix(matrix(rnorm(16), 4, 4))
+cacheSolve(bigMatrix)
